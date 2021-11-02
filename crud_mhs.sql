f201355a0ed8b8c1df884ab3815d0d8df45a716d
WindaSofia <92179183+WindaSofia@users.noreply.github.com>
2021-11-02 21:35:39 +0700

    Add files via upload

---
+++ crud_mhs.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2021 at 10:28 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswacrud1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mhs`
+--
+
+CREATE TABLE `tbl_mhs` (
+  `id` int(11) NOT NULL,
+  `nim` char(11) NOT NULL,
+  `namamhs` varchar(100) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `kota` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_mhs`
+--
+
+INSERT INTO `tbl_mhs` (`id`, `nim`, `namamhs`, `jk`, `alamat`, `kota`, `email`, `foto`) VALUES
+(1, '23005617', 'Sofia Putri', 'P', 'Jalan Salak No.5 RT 04 RW 05, Desa Sumberagung', 'Jember', 'sofiaputri@gmail.com', '344-☁️💗.jpg'),
+(2, '200035671', 'Reynisa Putri', 'P', 'Jalan Mawar No.9 Desa Sumberagung, Kecamatan Sumberbaru', 'Jember', 'reynisaputri@gmail.com', '164-☁️💗.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
